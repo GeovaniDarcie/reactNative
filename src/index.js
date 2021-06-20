101c71435454ad6f9def53f0f9c7ff0a7d778ace
geovaniDarcie <geovani_darcie@hotmail.com>
2021-06-20 12:51:28 -0300

    youtube clone

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import { View, Text, ScrollView, StyleSheet, Image } from 'react-native';
+import { View, Text, ScrollView, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Video from 'react-native-video';
+import { Container, Content, List, ListItem } from 'native-base';
 
 import Icon from 'react-native-vector-icons/FontAwesome';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
-const app = () => {
+const ListaVideos = ({ navigation }) => {
     return (
         <View style={{ flex: 1 }}>
             <ScrollView>
@@ -14,28 +20,81 @@ const app = () => {
                     />
 
                     <View style={styles.headerIcons}>
-                        <Icon name="bell" style={{ margin: 10 }} size={18} />
+                        <Icon name="bell" style={{ margin: 10 }} size={16} />
                         <Icon name="search" style={{ margin: 10 }} size={18} />
                         <Icon name="home" style={{ margin: 10 }} size={18} />
                     </View>
-
                 </View>
-                <Image
-                    style={styles.video}
-                    source={require('../video1.jpeg')}
-                />
-                <Image
-                    style={styles.video}
-                    source={require('../video2.jpeg')}
-                />
-                <Image
-                    style={styles.video}
-                    source={require('../video3.jpeg')}
-                />
+                <Content>
+                    <TouchableOpacity onPress={() => {
+                        navigation.navigate('Video', {
+                            external: true,
+                            videoUrl: 'https://www.youtube.com/embed/IU3KH0a4IUg'
+                        })
+                    }}>
+                        <Image style={styles.video}
+                            source={require('../video1.jpeg')}
+                        />
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => {
+                        navigation.navigate('Video', {
+                            external: false,
+                            videoUrl: require('../coelho.mp4')
+                        })
+                    }}>
+                        <Image style={styles.video}
+                            source={require('../video2.jpeg')}
+                        />
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => {
+                        navigation.navigate('Video', {
+                            external: true,
+                            videoUrl: 'https://youtu.be/zZdLSGytRc0'
+                        })
+                    }}>
+                        <Image style={styles.video}
+                            source={require('../video3.jpeg')}
+                        />
+                    </TouchableOpacity>
+
+                </Content>
             </ScrollView>
-            <View style={styles.footer}><Text>Footer</Text></View>
+            <View style={styles.footer}>
+                <Icon name="home" style={{ margin: 10 }} size={20} />
+                <Icon name="compass" style={{ margin: 10 }} size={20} />
+                <AntDesign name="pluscircleo" style={{ margin: 10 }} size={25} />
+                <MaterialIcons name="app-registration" style={{ margin: 10 }} size={20} />
+                <MaterialIcons name="library-add" style={{ margin: 10 }} size={20} />
+            </View>
         </View>
     );
+}
+
+const VideoPlayer = ({ navigation, route }) => {
+    const { videoUrl, external } = route.params;
+    return (
+        <Container style={styles.container}>
+            <Video
+                controls
+                resizeMode="contain"
+                source={external ? { uri: videoUrl } : videoUrl}
+                style={styles.backgroundVideo}
+            />
+        </Container>
+    )
+}
+
+const Stack = createStackNavigator();
+
+const app = () => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="Youtube" component={ListaVideos} />
+                <Stack.Screen name="Video" component={VideoPlayer} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
 }
 
 const styles = StyleSheet.create({
@@ -67,16 +126,34 @@ const styles = StyleSheet.create({
 
     video: {
         width: '100%',
-        height: 300
+        height: 280
     },
 
     footer: {
+        flexDirection: 'row',
         fontSize: 10,
         margin: 1,
         borderTopWidth: 1,
         borderBottomWidth: 1,
         borderColor: '#eeeeee',
         padding: 1,
+        justifyContent: 'space-around'
+    },
+    backgroundVideo: {
+        position: 'absolute',
+        height: 500,
+        width: '100%',
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+    },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 280,
+        backgroundColor: 'black'
     }
 });
 
